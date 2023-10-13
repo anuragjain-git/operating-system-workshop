feb953f3dab0aa5b2a5e1a561f40203e11fb48c2
Anurag Jain <98457054+anuragjain-git@users.noreply.github.com>
2023-10-13 17:44:56 +0530

    uploaded malloc function code

---
+++ mallocfunction.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <malloc.h>
+
+void display(int *, int);
+
+int main() {
+	int n;
+	int *a;
+	printf("enter the size of array: ");
+	scanf("%d",&n);
+	a = (int *) malloc(n*sizeof(int));
+	
+	printf("Enter elements: ");
+	for(int i =0; i < n; i++) {
+		scanf("%d",&a[i]);
+	}
+	display(a,n);
+	
+	return 0;
+}
+
+void display(int *a, int n) {
+	for(int i = 0;i < n; i++)
+		printf("%d ",a[i]);
+	printf("\n");
+}
