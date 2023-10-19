1078c843b7707900272423c3c4bf48c4606ac996
Anurag Jain <98457054+anuragjain-git@users.noreply.github.com>
2023-10-19 13:36:49 +0530

    Update matrixmul.c

--- matrixmul.c
+++ matrixmul.c
@@ -6,7 +6,7 @@ int** mul(int row1, int col1, int col2, int **ptr1, int **ptr2) {
 	int **res = (int **) malloc(sizeof(int *) * row1);
 	
 	for(int i = 0; i < row1; i++) {
-		res[i] = (int *) malloc(sizeof(int) * col1);
+		res[i] = (int *) malloc(sizeof(int) * col2);
 	}
 	
 	for(int i=0;i<row1;i++){
@@ -41,40 +41,43 @@ int main() {
 	printf("Enter row and col for second matrix : ");
 	scanf("%d%d", &row2, &col2);
 	
-	ptr1 = (int **) malloc(sizeof(int *) * row1);
-	ptr2 = (int **) malloc(sizeof(int *) * row2);
+	if (row2 == col1 && row1 == col2) {
 	
-	for(int i = 0; i < row1; i++) {
-		ptr1[i] = (int *) malloc(sizeof(int) * col1);
-	}
+		ptr1 = (int **) malloc(sizeof(int *) * row1);
+		ptr2 = (int **) malloc(sizeof(int *) * row2);
 	
-	for(int i = 0; i < row2; i++) {
-		ptr2[i] = (int *) malloc(sizeof(int) * col2);
-	}
-	
-	printf("Enter values for matrix1 :\n");
-	for(int i = 0; i < row1; i++) {
-		for(int j = 0; j < col1; j++) {
-			scanf("%d", &ptr1[i][j]);
+		for(int i = 0; i < row1; i++) {
+			ptr1[i] = (int *) malloc(sizeof(int) * col1);
 		}
-	}
 	
-	printf("Enter values for matrix2 : \n");
-	for(int i = 0; i < row2; i++) {
-		for(int j = 0; j < col2; j++) {
-			scanf("%d", &ptr2[i][j]);
+		for(int i = 0; i < row2; i++) {
+			ptr2[i] = (int *) malloc(sizeof(int) * col2);
 		}
-	}
-      	
-      	res = mul(row1, col1, col2, ptr1, ptr2);
-      	displayres(res, row1, col2); 	
-      	
+	
+		printf("Enter values for matrix1 :\n");
+		for(int i = 0; i < row1; i++) {
+			for(int j = 0; j < col1; j++) {
+				scanf("%d", &ptr1[i][j]);
+			}
+		}
+	
+		printf("Enter values for matrix2 : \n");
+		for(int i = 0; i < row2; i++) {
+			for(int j = 0; j < col2; j++) {
+				scanf("%d", &ptr2[i][j]);
+			}
+		}
+   
+      		res = mul(row1, col1, col2, ptr1, ptr2);
+      		displayres(res, row1, col2);
+     
+      	}
+      	 
+      	else {
+      		printf("Invalid matrix size.\n");
+      	}
+
      }
-     
-     
-     
-     
-     
      
      
      
